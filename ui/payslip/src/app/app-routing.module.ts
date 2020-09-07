b85fcb002e6d7070f9b0511580c7c3e3a455c4ce
KyoOnizuka <30726685+KyoOnizuka@users.noreply.github.com>
2020-09-07 23:27:55 +0700

    no message

--- ui/payslip/src/app/app-routing.module.ts
+++ ui/payslip/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule  } from '@angular/platform-browser';
+import { AuthGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./Admin/admin.module').then(m => m.AdminModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'employee',
+    loadChildren: () => import('./Employee/employee.Module').then(m => m.EmployeeModule),
+    canLoad: [AuthGuard]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- ui/payslip/src/app/app.module.ts
+++ ui/payslip/src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { DatePipe } from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CookieInterceptor } from './auth/cookie.interceptor';
+import { MaterialLibraryModule } from './shared/material/material-library.module';
+import { AdminModule } from './Admin/admin.module';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
+import { EmployeeModule } from './Employee/employee.Module';
+import { AddTimeWorkComponent } from './Employee/list-work/add-time-work/add-time-work.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTimeWorkComponent,
+    SideBarComponent,
+    NavBarComponent,
   ],
   imports: [
+    AdminModule,
+    BrowserAnimationsModule,
+    MaterialLibraryModule,
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    AuthModule,
+    EmployeeModule,
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    DatePipe,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CookieInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
